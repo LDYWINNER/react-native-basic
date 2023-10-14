5e292b23d07bb77413ad4f94c7e2f091ec51eb21
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-10-14 13:43:55 -0400

    ADD - AppLoading preset

--- RN-GeneralConcepts/movie-app/App.js
+++ RN-GeneralConcepts/movie-app/App.js
@@ -1,5 +1,22 @@
-import React from "react";
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const onFinish = () => {
+    setReady(true);
+  };
+  const startLoading = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
   return null;
 }
