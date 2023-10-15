a76ef73428498d0b519da535388639297c516269
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-10-15 17:39:30 -0400

    ADD - bottom tab navigator

--- RN-GeneralConcepts/movie-app/App.js
+++ RN-GeneralConcepts/movie-app/App.js
@@ -3,6 +3,8 @@ import * as Font from "expo-font";
 import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
@@ -34,5 +36,9 @@ export default function App() {
       />
     );
   }
-  return null;
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
